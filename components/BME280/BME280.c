e46ba4f71ad52085f03c158c2f1f894d13545e6a
franmartinv <95523823+franmartinv@users.noreply.github.com>
2022-05-19 12:49:14 +0200

    Update BME280.c

--- components/BME280/BME280.c
+++ components/BME280/BME280.c
@@ -3,7 +3,7 @@
 *	-------------------------------------------
 *
 *	***************************************************
-*	*		  		FAST USER GUIDE 		  	  	  *
+*	*		 FAST USER GUIDE 		  *
 *	***************************************************
 *  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *
@@ -49,12 +49,12 @@ extern "C" {
 
 
 /*
- * @brief Data lecture on BMP280
+ * @brief 		Data lecture on BMP280
  *
- * @param[in]	BMP280_command	:	(uint8_t)	command that we want to execute
- * @param[in]	size			:	(unsigned)	size of the number of bytes that we want to read
+ * @param[in]		BMP280_command		:	(uint8_t)	command that we want to execute
+ * @param[in]		size			:	(unsigned)	size of the number of bytes that we want to read
  *
- * @param[out]	*buffer_out		:	(uint8_t)	pointer to array where the functions save the lecture of BMP280_command
+ * @param[out]		*buffer_out		:	(uint8_t)	pointer to array where the functions save the lecture of BMP280_command
  *
  * @return		Response of esp_err_t function type
  *
@@ -86,9 +86,9 @@ esp_err_t BMP280_read(uint8_t * buffer_out, uint8_t BMP280_command, unsigned siz
 
 
 /*
- * @brief	Data writting on BMP280
+ * @brief		Data writting on BMP280
  *
- * @param[in]	BMP280_command	:	(uint8_t)	command that we want to execute
+ * @param[in]		BMP280_command	:	(uint8_t)	command that we want to execute
  *
  * @return		Response of esp_err_t function type
  *
@@ -110,10 +110,10 @@ esp_err_t BMP280_write_command(uint8_t BMP280_command)
 
 
 /*
- * @brief	Data writting on BMP280
+ * @brief		Data writting on BMP280
  *
- * @param[in]	BMP280_register			:	(uint8_t)	address of the register where we want to write
- * @param[in]	BMP280_register_value	:	(uint8_t)	value of the register that we want to write
+ * @param[in]		BMP280_register			:	(uint8_t)	address of the register where we want to write
+ * @param[in]		BMP280_register_value		:	(uint8_t)	value of the register that we want to write
  *
  * @return		Response of esp_err_t function type
  *
@@ -136,9 +136,9 @@ esp_err_t BMP280_write_register(uint8_t BMP280_register, uint8_t BMP280_register
 
 
 /**
- * @brief	Calibration coefficients of temperature, humidity and pressure
+ * @brief		Calibration coefficients of temperature, humidity and pressure
  *
- * @param[ou]	*NMV_coefs	:	(BMP280_calib_t)	struct where we are going to save the calibration parameters for compensate readings
+ * @param[ou]		*NMV_coefs	:	(BMP280_calib_t)	struct where we are going to save the calibration parameters for compensate readings
  *
  * @return		Response of esp_err_t function type
  *
@@ -153,7 +153,7 @@ esp_err_t BMP280_read_calib_coeff(BMP280_calib_t *NVM_coefs)
 
 	for(i = 0; i < 12; i++) {
 		ret = BMP280_read(buffer_out, BMP280_REG_CALIB + (2*i), 2);		// The temperature and pressure registers are separated
-																		// by 2 directions, so, i will read 2 by 2
+											// by 2 directions, so, i will read 2 by 2
 		if(ret != ESP_OK) {
 			printf("ERROR writing calibration coefficient number %d\n", i);
 		}
@@ -305,7 +305,7 @@ void hal_i2c_init()
 
 
 /**
- *	@brief	Slave initialation
+ *	@brief		Slave initialation
  *
  *	@param[in]	*NVM_coef	:	(BMP280_calib_t)	struct where we are going to save the calibration parameters for compensate readings
  *
@@ -414,12 +414,12 @@ int BMP280_init(BMP280_calib_t *NVM_coef, int sensor)
 
 
 /**
- * @brief	Temperature compensation in int-variable for BMP280
+ * @brief		Temperature compensation in int-variable for BMP280
  *
- * @param[in]	adc_tempe	:	(int32_t)			raw adc temperature data
- * @param[in]	*NVM_coef	:	(BMP280_calib_t)	struct where we are going to save the calibration parameters for compensate readings
+ * @param[in]		adc_tempe	:	(int32_t)			raw adc temperature data
+ * @param[in]		*NVM_coef	:	(BMP280_calib_t)	struct where we are going to save the calibration parameters for compensate readings
  *
- * @return	t_fine	:	(int32_t)	compensate t_fine temperature variable
+ * @return		t_fine		:	(int32_t)	compensate t_fine temperature variable
  *
  */
 int32_t BMP280_int_temperature_compensation(int32_t adc_tempe, BMP280_calib_t *NVM_coef)
@@ -437,11 +437,11 @@ int32_t BMP280_int_temperature_compensation(int32_t adc_tempe, BMP280_calib_t *N
 
 
 /**
- * @brief	Pressure compensation for BMP280
+ * @brief		Pressure compensation for BMP280
  *
- * @param[in]	adc_pres	:	(int32_t)			raw adc pressure data
- * @param[in]	*NVM_coef	:	(BMP280_calib_t)	struct where we are going to save the calibration parameters for compensate readings
- * @param[in]	t_fine		:	(int32_t)			compensate t_fine temperature variable
+ * @param[in]		adc_pres		:	(int32_t)			raw adc pressure data
+ * @param[in]		*NVM_coef		:	(BMP280_calib_t)		struct where we are going to save the calibration parameters for compensate readings
+ * @param[in]		t_fine			:	(int32_t)			compensate t_fine temperature variable
  *
  * @return		p			:	(uint32_t)			compensate pressure
  *
@@ -472,11 +472,11 @@ uint32_t BMP280_int_pressure_compensation(int32_t adc_pres, BMP280_calib_t *NVM_
 
 
 /**
- * @brief	Compensate humidity for BME280
+ * @brief		Compensate humidity for BME280
  *
- * @param[in]	adc_hum		:	(int32_t)			raw adc humidity data
- * @param[in]	*NVM_coef	:	(BMP280_calib_t)	struct where we are going to save the calibration parameters for compensate readings
- * @param[in]	t_fine		:	(int32_t)			compensate t_fine temperature variable
+ * @param[in]		adc_hum			:	(int32_t)			raw adc humidity data
+ * @param[in]		*NVM_coef		:	(BMP280_calib_t)		struct where we are going to save the calibration parameters for compensate readings
+ * @param[in]		t_fine			:	(int32_t)			compensate t_fine temperature variable
  *
  * @return		p			:	(uint32_t)			compensate humidity
  *
@@ -499,13 +499,13 @@ uint32_t BMP280_int_humidity_compensation(int32_t adc_hum, BMP280_calib_t *NVM_c
 
 
 /**
- * @brief	Get final data for BME/P 280
+ * @brief		Get final data for BME/P 280
  *
- * @param[in]	*temperature	:	(float)				pointer to temperature float variable
- * @param[in]	*pressure		:	(float)				pointer to pressure float variable
- * @param[in]	*humidity		:	(float)				pointer to humidity float variable
- * @param[in]	sensor			:	(int)				type of sensor that we are using. Choose 1 for BME280 or 0 for BMP280
- * @param[in]	*NVM_coef		:	(BMP280_calib_t)	struct where we are going to save the calibration parameters for compensate readings
+ * @param[in]		*temperature		:	(float)					pointer to temperature float variable
+ * @param[in]		*pressure		:	(float)				pointer to pressure float variable
+ * @param[in]		*humidity		:	(float)				pointer to humidity float variable
+ * @param[in]		sensor			:	(int)				type of sensor that we are using. Choose 1 for BME280 or 0 for BMP280
+ * @param[in]		*NVM_coef		:	(BMP280_calib_t)		struct where we are going to save the calibration parameters for compensate readings
  *
  */
 void BMP280_final_data(float *temperature, float *pressure, float *humidity, int sensor, BMP280_calib_t *NVM_coef)
